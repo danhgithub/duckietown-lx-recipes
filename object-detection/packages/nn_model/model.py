2a72ccbb6ee8454ac205fe1017d778bcc412662e
Danh Nguyen <31724292+danhgithub@users.noreply.github.com>
2023-11-16 12:03:38 -0800

    Update model.py

--- object-detection/packages/nn_model/model.py
+++ object-detection/packages/nn_model/model.py
@@ -111,10 +111,10 @@ class Model:
     def __init__(self, weight_file_path: str):
         super().__init__()
         print("dtn ***************** weight_file_path = ",weight_file_path,"******************")
-        model = torch.hub.load("/yolov5", "custom", path=weight_file_path, source="local")
+        #model = torch.hub.load("/yolov5", "custom", path=weight_file_path, source="local")
         #model = torch.hub.load("/home/yolov5", "custom", path=weight_file_path, source="local")
         #model = torch.hub.load("danhgithub/yolov5", "custom", path=weight_file_path, source="github")
-        #model = torch.hub.load("danhgithub/yolov5d1", "custom", path=weight_file_path, source="github")
+        model = torch.hub.load("danhgithub/yolov5d1", "custom", path=weight_file_path, source="github")
         model.eval()
         print("dtn *****************")
         use_fp16: bool = JETSON_FP16 and get_device_hardware_brand() == DeviceHardwareBrand.JETSON_NANO
